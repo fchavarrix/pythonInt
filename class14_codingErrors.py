4e7b1072f75992d7063e520a55c07df6024d8a31
Felipe Chavarri <fchavarri@pucp.edu.pe>
2021-08-15 13:33:49 -0500

    Se agrego clase 14

---
+++ class14_codingErrors.py
@@ -0,0 +1,10 @@
+def run():
+    tree01 = """
+    Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>
+    ZeroDivisionError: division by zero
+    """
+    print (tree01)
+
+if __name__ == '__main__':
+    run()
